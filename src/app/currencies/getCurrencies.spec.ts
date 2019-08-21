d28e25a5c4a0c31d420ac26da161ec5d92403032
DiegoPerezVega <A01329098@itesm.mx>
2019-08-21 15:33:07 -0500

    currencies features

--- src/app/currencies/getCurrencies.spec.ts
+++ src/app/currencies/getCurrencies.spec.ts
@@ -1,7 +1,8 @@
-import{getCurrencies} from ',getCurrencies';
-describe(’currencies’, () => {//currencies suite
-it(’shouldreturn the supported currencies’,() => {
-constresult = getCurrencies();
+import { getCurrencies } from './getCurrencies';
+
+describe ('currencies', () => { //currencies suite
+it('should return the supported currencies',() => {
+const result = getCurrencies();
 expect(result).toContain('USD');
 expect(result).toContain('AUD');
 expect(result).toContain('EUR');
